5330185417fdb5d7bb75ec763812f2f703d64017
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-06-10 13:21:46 -0700

    add logic for each symbol
    co-authored-by: Michael Reiersgaard <michael.reiersgaard@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -18,11 +18,29 @@ function romanNum(number) {
   //   }
   // }
   while (number > 0) {
-    if (number >= 1000) {
+    if (number >= values[6]) {
       output += symbols[6];
-      number -= 1000; 
+      number -= values[6]; 
+    } else if (number >= values[5]) {
+      output += symbols[5];
+      number -= values[5];
+    } else if (number >= values[4]) {
+      output += symbols[4];
+      number -= values[4];
+    } else if (number >= values[3]) {
+      output += symbols[3];
+      number -= values[3];
+    } else if (number >= values[2]) {
+      output += symbols[2];
+      number -= values[2];
+    } else if (number >= values[1]) {
+      output += symbols[1];
+      number -= values[1];
+    } else if (number >= values[0]) {
+      output += symbols[0];
+      number -= values[0];
     }
   }
-  console.log(output);
-  console.log(number);
+  console.log("Output: "+output);
+  console.log("Number: "+number);
 }
